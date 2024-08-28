eef82707c7130e450c6b850612ec5ec3f37260ca
岩田寛大 <iwatahiroshidai@TEC-024-0001.local>
2024-08-28 15:42:09 +0900

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: 'Example User',
+             email: 'example@railstutorial.org',
+             password: 'foobarpaw',
+             password_confirmation: 'foobarpaw',
+             admin: true)
+99.times do |n|
+  test_name = Faker::Name.name
+  test_email = "example-#{n + 1}@railstutorial.org"
+  test_password = 'password'
+  User.create!(name: test_name,
+               email: test_email,
+               password: test_password,
+               password_confirmation: test_password)
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,12 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
-require "minitest/reporters"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'minitest/reporters'
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
@@ -26,6 +26,6 @@ end
 
 class ActionDispatch::IntegrationTest
   def log_in_as(user, password: 'password', remember_me: '1')
-    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me } }
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me} } # rubocop:disable Style/HashSyntax,Layout/SpaceInsideHashLiteralBraces
   end
 end
